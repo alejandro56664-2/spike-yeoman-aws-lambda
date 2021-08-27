0e65af0e3531e1bf0bee3066253bc10a122922b8
Jheison Alejandro Morales Vasquez <jheison.morales@avaldigitallabs.com>
2021-08-27 15:42:50 -0500

    :bug: se arregla functionName en paramKeys

--- generator-lambda-java-mn/generators/config-manager-ssm/templates/src/main/java/pkgBase/enums/ParamKey.java
+++ generator-lambda-java-mn/generators/config-manager-ssm/templates/src/main/java/pkgBase/enums/ParamKey.java
@@ -8,10 +8,10 @@ import lombok.Getter;
 public enum ParamKey {
   // TODO: por favor recuerde que debe tener las claves necesarias en el AWS System Manager.
   /**
-   * Recuerde usar siempre el formato (en la enumeración) path.to.key -> (en AWS SSM) path/to/key
+   * Recuerde usar siempre el formato (en la enumeración) path.to.key a (en AWS SSM) path/to/key
    * También tenga presente que es una buena idea siempre usar el repo de parametros.
    */
-  FUNCTION_NAME("function.name");
+  FUNCTION_NAME("<%= functionName %>");
 
   private final String value;
 }
